5593057523c536d3bfffe611aad3608c0ec44fb5
Breno Viana <brenomfviana@gmail.com>
2020-06-07 17:04:44 -0300

    fixes solver tests

--- src/tests/solver.rs
+++ src/tests/solver.rs
@@ -30,12 +30,12 @@ fn backtracking_3_clique_i() {
   clique.insert_edge((1, 2));
   clique.insert_edge((1, 3));
   clique.insert_edge((3, 2));
-  let result = solver::solve(&clique, &Solver::Backtracking).unwrap();
   let mut clique_nodes = clique.nodes(); clique_nodes.sort();
-  let mut result_nodes = result.nodes(); result_nodes.sort();
-  assert_eq!(clique_nodes, result_nodes);
   let mut clique_edges = clique.edges(); clique_edges.sort();
+  let result = solver::solve(&clique, &Solver::Backtracking).unwrap();
+  let mut result_nodes = result.nodes(); result_nodes.sort();
   let mut result_edges = result.edges(); result_edges.sort();
+  assert_eq!(clique_nodes, result_nodes);
   assert_eq!(clique_edges, result_edges);
 }
 
@@ -44,19 +44,27 @@ fn backtracking_3_clique_ii() {
   let mut graph = Graph::new(5);
   graph.insert_edge((1, 2));
   graph.insert_edge((1, 3));
-  graph.insert_edge((3, 2));
   graph.insert_edge((2, 4));
+  graph.insert_edge((3, 2));
   graph.insert_edge((3, 4));
   graph.insert_edge((1, 5));
-  let mut clique = Graph::new(3);
-  clique.insert_edge((1, 2));
-  clique.insert_edge((1, 3));
-  clique.insert_edge((3, 2));
   let result = solver::solve(&graph, &Solver::Backtracking).unwrap();
-  let mut clique_nodes = clique.nodes(); clique_nodes.sort();
   let mut result_nodes = result.nodes(); result_nodes.sort();
-  assert_eq!(clique_nodes, result_nodes);
-  let mut clique_edges = clique.edges(); clique_edges.sort();
   let mut result_edges = result.edges(); result_edges.sort();
+  let mut clique = Graph::new(4);
+  if result_nodes.contains(&1) {
+    clique.remove_node(4);
+    clique.insert_edge((1, 2));
+    clique.insert_edge((1, 3));
+    clique.insert_edge((3, 2));
+  } else {
+    clique.remove_node(1);
+    clique.insert_edge((2, 3));
+    clique.insert_edge((2, 4));
+    clique.insert_edge((3, 4));
+  }
+  let mut clique_nodes = clique.nodes(); clique_nodes.sort();
+  let mut clique_edges = clique.edges(); clique_edges.sort();
+  assert_eq!(clique_nodes, result_nodes);
   assert_eq!(clique_edges, result_edges);
 }
